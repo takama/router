4b73d480c48a7aae1c5a0a8251d55810df528f60
Igor Dolzhikov <bluesriverz@gmail.com>
2016-10-22 16:23:21 +0700

    Google json structure

--- control.go
+++ control.go
@@ -36,8 +36,18 @@ type Control struct {
 	// Code of HTTP status
 	code int
 
-	// CompactJSON propery defines JSON output format (default is not compact)
+	// compactJSON propery defines JSON output format (default is not compact)
 	compactJSON bool
+
+	// if used, json header shows meta data
+	useMetaData bool
+
+	// header with metadata
+	header Header
+
+	// errors
+	errorHeader ErrorHeader
+	errors      []Error
 
 	// Params is set of parameters
 	Params []Param
@@ -52,11 +62,32 @@ type Param struct {
 	Value string
 }
 
-// Header is used to prepare a JSON header with duration triggered by UserTimer() method
+// Header is used to prepare a JSON header with meta data
 type Header struct {
-	Duration time.Duration `json:"duration"`
-	Took     string        `json:"took"`
-	Data     interface{}   `json:"data"`
+	Duration   time.Duration `json:"duration,omitempty"`
+	Took       string        `json:"took,omitempty"`
+	APIVersion string        `json:"apiVersion,omitempty"`
+	Context    string        `json:"context,omitempty"`
+	ID         string        `json:"id,omitempty"`
+	Method     string        `json:"method,omitempty"`
+	Params     interface{}   `json:"params,omitempty"`
+	Data       interface{}   `json:"data,omitempty"`
+	Error      interface{}   `json:"error,omitempty"`
+}
+
+type ErrorHeader struct {
+	Code    uint16      `json:"code,omitempty"`
+	Message string      `json:"message,omitempty"`
+	Errors  interface{} `json:"errors,omitempty"`
+}
+
+type Error struct {
+	Domain       string `json:"domain,omitempty"`
+	Reason       string `json:"reason,omitempty"`
+	Location     string `json:"location,omitempty"`
+	LocationType string `json:"locationType,omitempty"`
+	ExtendedHelp string `json:"extendedHelp,omitempty"`
+	SendReport   string `json:"sendReport,omitempty"`
 }
 
 // Get returns the first value associated with the given name.
@@ -85,12 +116,61 @@ func (c *Control) Code(code int) *Control {
 }
 
 // CompactJSON change JSON output format (default mode is false)
-func (c *Control) CompactJSON(mode bool) {
+func (c *Control) CompactJSON(mode bool) *Control {
 	c.compactJSON = mode
+	return c
+}
+
+// UseMetaData shows meta data in JSON Header
+func (c *Control) UseMetaData() *Control {
+	c.useMetaData = true
+	return c
+}
+
+// APIVersion add API version meta data
+func (c *Control) APIVersion(version string) *Control {
+	c.useMetaData = true
+	c.header.APIVersion = version
+	return c
+}
+
+// Context add context meta data
+func (c *Control) Context(context string) *Control {
+	c.useMetaData = true
+	c.header.Context = context
+	return c
+}
+
+// ID add id meta data
+func (c *Control) ID(id string) *Control {
+	c.useMetaData = true
+	c.header.ID = id
+	return c
+}
+
+// Method add method meta data
+func (c *Control) Method(method string) *Control {
+	c.useMetaData = true
+	c.header.Method = method
+	return c
+}
+
+func (c *Control) produceError(code uint16, message string) *Control {
+	c.useMetaData = true
+	c.errorHeader.Code = code
+	c.errorHeader.Message = message
+	return c
+}
+
+func (c *Control) AddError(errors ...Error) *Control {
+	c.useMetaData = true
+	c.errors = append(c.errors, errors...)
+	return c
 }
 
 // UseTimer allow caalculate elapsed time of request handling
 func (c *Control) UseTimer() {
+	c.useMetaData = true
 	c.timer = time.Now()
 }
 
@@ -106,9 +186,23 @@ func (c *Control) Body(data interface{}) {
 			c.Writer.Header().Add("Content-type", MIMETEXT)
 		}
 	} else {
-		if !c.timer.IsZero() {
-			took := time.Now()
-			data = &Header{Duration: took.Sub(c.timer), Took: took.Sub(c.timer).String(), Data: data}
+		if c.useMetaData {
+			c.header.Data = data
+			if !c.timer.IsZero() {
+				took := time.Now()
+				c.header.Duration = took.Sub(c.timer)
+				c.header.Took = took.Sub(c.timer).String()
+			}
+			if len(c.Params) > 0 {
+				c.header.Params = c.Params
+			}
+			if c.errorHeader.Code != 0 || c.errorHeader.Message != "" || len(c.errors) > 0 {
+				if len(c.errors) > 0 {
+					c.errorHeader.Errors = c.errors
+				}
+				c.header.Error = c.errorHeader
+			}
+			data = c.header
 		}
 		var err error
 		if c.compactJSON {
