650c235b2fbe0015b87d5292adbead5b87283745
Igor Dolzhikov <bluesriverz@gmail.com>
2017-03-11 01:05:46 +0700

    Bumped version number to 0.6.3

--- router.go
+++ router.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package router 0.6.2 provides fast HTTP request router.
+Package router 0.6.3 provides fast HTTP request router.
 
 The router matches incoming requests by the request method and the path.
 If a handle is registered for this path and method, the router delegates the
